a448f957a3333abb61e0af20dd3adef35c72adf4
kaufon <kauanfvale@gmail.com>
2024-12-16 19:50:18 -0300

    [front]🛤️routes: add frontend routes

--- apps/web/src/app/router/index.ts
+++ apps/web/src/app/router/index.ts
@@ -1,15 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Hello from '@/ui/components/pages/Hello.vue'
+import DashboardPage from "@/ui/components/pages/dashboard/DashboardPage.vue";
+import LoginPage from "@/ui/components/pages/login/LoginPage.vue";
+import RegisterPage from "@/ui/components/pages/register/RegisterPage.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Hello,
-    },
-  ],
-})
-
-export default router
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: DashboardPage,
+		},
+		{
+			path: "/login",
+			name: "Login",
+			component: LoginPage,
+		},
+		{
+			path: "/register",
+			name: "Register",
+			component: RegisterPage,
+		},
+	],
+});
+router.beforeEach((to, from, next) => {
+	const token = localStorage.getItem("jwt");
+	if (to.name === "Login" || to.name === "Register") {
+		next();
+	} else if (token) {
+		next();
+	} else {
+		next("Login");
+	}
+});
+export default router;
