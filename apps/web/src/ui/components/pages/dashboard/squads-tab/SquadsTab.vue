e00c354727088d46988890640aeb698aecc4abc9
kaufon <kauanfvale@gmail.com>
2024-12-20 22:17:48 -0300

    [frontend]🧱components: squads tab

---
+++ apps/web/src/ui/components/pages/dashboard/squads-tab/SquadsTab.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid md:grid-cols-5 grid-row-5 gap-2  md:gap-24">
+    <div v-for="squad in squads" class="col-span-1" :key="squad.id">
+      <SquadCard :squad="squad" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  squads: SquadDto[]
+}>()
+import SquadCard from '@/ui/components/commons/SquadCard.vue';
+import type { SquadDto } from '@/@core';
+
+</script>
