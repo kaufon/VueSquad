1deac9a3d0460e49558d4f76120244a7bd5717a4
kaufon <kauanfvale@gmail.com>
2024-12-20 22:17:14 -0300

    [frontend]🧱components: logout button

---
+++ apps/web/src/ui/components/pages/dashboard/logout-button/LogoutButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <LogOutIcon class="size-8 text-white" v-on:click="logout" />
+  </div>
+</template>
+<script setup lang="ts">
+import router from '@/app/router';
+import { LogOutIcon } from 'lucide-vue-next';
+function logout() {
+
+  localStorage.removeItem("jwt")
+  router.push("/login")
+}
+</script>
